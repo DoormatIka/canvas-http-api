933312b399c68380ac60f9f23aa29a151748b3f0
DoormatIka <trashcanartistlily@gmail.com>
2023-08-10 01:17:55 +0900

    canvas calibration needed

--- index.ts
+++ index.ts
@@ -1,25 +1,30 @@
+import fs from "fs";
 import canvas from "canvas";
 import url from "url";
 import express from "express";
-import { wrapText } from "./src/clip.js";
+import fetch from "node-fetch";
 import { quote } from "./src/quote.js";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
+const c = canvas.createCanvas(1280, 720);
+const ctx = c.getContext("2d");
+
+app.use(express.json());
 
 const text = "Lorem ipsum dolor sit amet, "
     + "consectetur adipiscing elit. "
     + "Morbi rhoncus dignissim ligula eget gravida. "
     + "Integer nec euismod leo. Quisque pharetra. ";
+const overlay = await canvas.loadImage("resources/frame/overlay_gradient.png");
 
-const bg = await canvas.loadImage("resources/frame/background.png");
-const pfp = await canvas.loadImage("resources/frame/profile.png");
-const overlay = await canvas.loadImage("resources/frame/transparent_overlay.png")
-
-
-const buffer = quote(text, bg, pfp, overlay);
-
-app.post("/quote", (req, res) => {
+app.post("/quote", async (req, res) => {
+    const f = await fetch(req.body.url);
+    const convert = await fetch("http://localhost:3500/convertwebp", {
+        method: "POST",
+        body: await f.blob(),
+    });
+    const pfp = await canvas.loadImage(Buffer.from(await convert.arrayBuffer()));
+    const buffer = quote(c, ctx, req.body.text ?? text, req.body.author, pfp, overlay);
     res.send(buffer);
 });
 
