17dfe800018ff67cf0a561148e8d7b601c605806
DoormatIka <trashcanartistlily@gmail.com>
2023-12-01 00:31:13 +0800

    removed font resizing

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import canvas from "canvas";
 import fs from "fs";
 import express from "express";
-import {quote, quoteAttachment, Text} from "./src/quote.js";
 import {WEBPToCanvas, getImage} from "./src/helpers.js";
+import { Text, quote, quoteAttachment } from "./src/quote.js";
 
 const app = express();
 
@@ -17,38 +17,45 @@ app.post("/quote", async (req, res) => {
 	const c = canvas.createCanvas(1280, 720);
 	const ctx = c.getContext("2d");
 
-	const pfp = req.body.use_test_pfp
-		? test_pfp
-		: await canvas.loadImage(Buffer.from(await getImage(req.body.avatar_url)));
+	const use_test_pfp: boolean = req.body.use_test_pfp;
+	const pipe_to_file: boolean = req.body.pipe_to_file;
+	const show_bounding: boolean = req.body.show_bounding;
 
+	const text_field: string = req.body.text;
+	const author_field: string = req.body.author;
+	const avatar_url: string = req.body.avatar_url;
+
+	const pfp = use_test_pfp
+		? test_pfp
+		: await canvas.loadImage(Buffer.from(await getImage(avatar_url)));
+
+	// 100 max
 	const text: Text = {
-		text: req.body.text,
-		size: 50,
+		text: text_field.slice(0, 100),
 		x: 750,
 		y: 200,
 		w: Math.floor(c.width / 3),
-		h: (c.height / 2.5),
-		max_font_size: 70,
-		iterations: 50,
-		extras: "Bold",
+		h: Math.floor(c.height / 2.5),
+		style: "Bold 50px Times New Roman",
 	};
+	// 20 max
 	const author: Text = {
-		text: req.body.author,
-		size: 10,
+		text: author_field.slice(0, 20),
 		x: 900,
 		y: 600,
 		w: 300,
 		h: 50,
-		max_font_size: 30,
-		iterations: 10,
+		style: "30px Times New Roman"
 	};
 
 	const box_info = quote(ctx, text, author, pfp, overlay);
-	if (req.body.show_bounding) {
-		box_info.main_text.draw_bounding_box();
-		box_info.main_text.draw_true_bounding_box();
-		box_info.author_text.draw_bounding_box();
-		box_info.author_text.draw_true_bounding_box();
+	if (show_bounding) {
+		box_info.main_text
+			.draw_bounding_box()
+			.draw_true_bounding_box();
+		box_info.author_text
+			.draw_bounding_box()
+			.draw_true_bounding_box();
 	}
 
 	const buffer = c.toBuffer("image/jpeg", {
@@ -57,7 +64,7 @@ app.post("/quote", async (req, res) => {
 		chromaSubsampling: false
 	});
 
-	if (req.body.pipe_to_file) {
+	if (pipe_to_file) {
 		fs.writeFileSync("fff.jpeg", buffer);
 		res.send("");
 	} else {
@@ -82,53 +89,62 @@ app.post("/quote/img", async (req, res) => {
 	const c = canvas.createCanvas(1280, 720);
 	const ctx = c.getContext("2d");
 
-
-	const pfp = req.body.use_test_pfp
-		? test_pfp
-		: await canvas.loadImage(Buffer.from(await getImage(req.body.avatar_url)));
+	const use_test_pfp: boolean = req.body.use_test_pfp;
+	const pipe_to_file: boolean = req.body.pipe_to_file;
+	const show_bounding: boolean = req.body.show_bounding;
 
 	const mimetype: string = req.body.mimetype;
-	const source_image = Buffer.from(await getImage(req.body.attachment_url));
+	const text_field: string = req.body.text;
+	const author_field: string = req.body.author;
+	const avatar_url: string = req.body.avatar_url;
+	const attachment_url: string = req.body.attachment_url;
+	const attachment_width: number = req.body.attachment_width;
+	const attachment_height: number = req.body.attachment_height;
+
+	const pfp = use_test_pfp
+		? test_pfp
+		: await canvas.loadImage(Buffer.from(await getImage(avatar_url)));
+
+	const source_image = Buffer.from(await getImage(attachment_url));
 	const attachment = mimetype.includes("image/webp")
-		? await WEBPToCanvas(source_image, req.body.attachment_width, req.body.attachment_height)
-		: await canvas.loadImage(Buffer.from(await getImage(req.body.attachment_url)))
+		? await WEBPToCanvas(source_image, attachment_width, attachment_height)
+		: await canvas.loadImage(source_image)
 
 	const text: Text = {
-		text: req.body.text,
-		size: 20,
+		text: text_field.slice(0, 100),
 		x: 800,
 		y: 500,
 		w: 400,
 		h: 100,
-		max_font_size: 40,
-		iterations: 25,
-		extras: "Bold",
+		style: "Bold 30px Times New Roman",
 	};
 	const author: Text = {
-		text: req.body.author,
-		size: 20,
+		text: author_field.slice(0, 20),
 		x: 1000,
 		y: 600,
 		w: 200,
 		h: 60,
-		max_font_size: 20,
-		iterations: 10,
+		style: "20px Times New Roman",
 	}
 
 	const box_info = quoteAttachment(c, ctx, text, author, pfp, overlay, attachment);
-	if (req.body.show_bounding) {
-		box_info.main_text.draw_bounding_box();
-		box_info.main_text.draw_true_bounding_box();
-		box_info.author_text.draw_bounding_box();
-		box_info.author_text.draw_true_bounding_box();
+	if (show_bounding) {
+		box_info.main_text
+			.draw_bounding_box()
+			.draw_true_bounding_box();
+		box_info.author_text
+			.draw_bounding_box()
+			.draw_true_bounding_box();
 	}
+	box_info.main_text.render();
+	box_info.author_text.render();
 
 	const buffer = c.toBuffer("image/jpeg", {
 		quality: 0.9,
 		progressive: false,
 		chromaSubsampling: false
 	})
-	if (req.body.pipe_to_file) {
+	if (pipe_to_file) {
 		fs.writeFileSync("fff_attachment.jpeg", buffer);
 		res.send("");
 	} else {
--- src/quote.ts
+++ src/quote.ts
@@ -1,4 +1,3 @@
-import {throws} from "assert";
 import canvas from "canvas";
 
 type Center = "center" | "right" | "left";
@@ -12,14 +11,12 @@ type WordCoords = {
 };
 export type Text = {
 	text: string,
-	size: number,
 	x: number,
 	y: number,
 	w: number,
 	h: number,
-	max_font_size: number,
-	iterations: number,
-	extras?: string,
+	color?: string,
+	style?: string,
 };
 
 export class TextArea {
@@ -28,7 +25,14 @@ export class TextArea {
 	private relative_text_height: number = 0;
 	private absolute_text_y: number = 0;
 	private absolute_text_x: number = 0;
-	
+	private coords: WordCoords[][] = [];
+	private previous_style: {
+		fillStyle?: string | CanvasGradient | CanvasPattern,
+		lineWidth?: number,
+		strokeStyle?: string | CanvasGradient | CanvasPattern,
+		font?: string,
+	} = {};
+
 	constructor(
 		private ctx: canvas.CanvasRenderingContext2D,
 		private text: string,
@@ -39,7 +43,27 @@ export class TextArea {
 		private will_center_x: boolean = false,
 		private will_center_y: boolean = true,
 	) {}
-	public set_text(text: string) { this.text = text }
+	public set_text(text: string) {this.text = text}
+	public get_coords() {return this.coords}
+	public store_styles() {
+		if (typeof this.ctx.fillStyle === "string") {
+			this.previous_style.fillStyle = this.ctx.fillStyle.toString();
+		} else {
+			this.previous_style.fillStyle = Object.assign({}, this.ctx.fillStyle);
+		}
+		if (typeof this.ctx.strokeStyle === "string") {
+			this.previous_style.strokeStyle = this.ctx.strokeStyle.toString();
+		} else {
+			this.previous_style.strokeStyle = Object.assign({}, this.ctx.strokeStyle);
+		}
+		this.previous_style.lineWidth = this.ctx.lineWidth.valueOf();
+		this.previous_style.font = this.ctx.font.toString();
+
+		return this;
+	}
+	public get_previous_style() {
+		return this.previous_style;
+	}
 	/*
 		* O(n^2) kinda algorithm.
 		* Wraps words via arrays, and measuring every word with canvas context.
@@ -49,7 +73,6 @@ export class TextArea {
 	public wrap_words() {
 		this.ctx.textBaseline = "top";
 
-		const coords: WordCoords[][] = [];
 		const paragraph = this.text.split("\n").map(c => c.split(/(?<=[ ])/g));
 
 		const y_adjust = 1.2;
@@ -65,7 +88,7 @@ export class TextArea {
 
 			for (const word of sentence) {
 				const local_text_metrics = this.ctx.measureText(word);
-				
+
 				const absolute_text_width = this.x + this.relative_text_width;
 
 				const absolute_word_x_offset_end = absolute_word_x_offset + local_text_metrics.width;
@@ -89,74 +112,13 @@ export class TextArea {
 			}
 			absolute_word_y_offset += global_text_metrics.actualBoundingBoxDescent / y_adjust;
 
-			coords.push(local_coords);
+			this.coords.push(local_coords);
 			this.relative_text_height = absolute_word_y_offset.valueOf() - this.y;
 		}
-		
-		return coords;
+
+		return this;
 	}
-	/*
-		* Resizing fonts to f
-		*
-		*/
-	public auto_font_resize(iterations: number, max_font_size: number) {
-		const previousFont = this.ctx.font.toString();
-		const previousFontSizeMatch = previousFont.match(/(\d+)px/)?.[1];
-		const garbageStyle = previousFont.replace(`${previousFontSizeMatch}px`, "()");
-
-		let absolute_text_y_limit = this.absolute_text_y + this.relative_text_height;
-		let absolute_text_x_limit = this.absolute_text_x + this.relative_text_width;
-		const absolute_y_limit = this.y + this.height;
-		const absolute_x_limit = this.x + this.width;
-
-		let previousFontSize = previousFontSizeMatch
-			? Number(previousFontSizeMatch)
-			: 20;
-
-		let wrapped: WordCoords[][] = [];
-
-		// a bodge to avoid the bouncing font sizes (50 => 49 => 50 => 49 ...)
-		for (let i = 0; i < iterations; i++) { 
-			if (previousFontSize > max_font_size) {
-				break;
-			}
-
-			this.ctx.font = garbageStyle.replace("()", `${previousFontSize}px`);
-
-			// if the text box's Y limit exceeds the parent box's Y, make the font size smaller
-			if (absolute_text_y_limit >= absolute_y_limit) {
-				previousFontSize -= 5;
-				wrapped = this.wrap_words();
-				absolute_text_y_limit = this.absolute_text_y + this.relative_text_height;
-				continue;
-			}
-			// +100 as an attempt to loosen the restrictions to activate this if statement
-			//
-			// if text box's Y limit does not exceed the parent box's Y limit AND
-			// 		if text box's X limit does not exceed the parent box's X limit:
-			// 				make the font size bigger
-			if (absolute_text_y_limit + 100 < absolute_y_limit 
-					&& absolute_text_x_limit < absolute_x_limit) {
-				previousFontSize += 5;
-				wrapped = this.wrap_words();
-				absolute_text_x_limit = this.absolute_text_x + this.relative_text_width;
-				absolute_text_y_limit = this.absolute_text_y + this.relative_text_height;
-				continue;
-			}
-
-			break;
-		}
-		this.ctx.font = previousFont.replace(`${previousFontSizeMatch}px`, "()");
-		// console.log(this.ctx.font, previousFontSize);
-		return {wrapped, previousFontSize};
-	}
-	public adjust_text_for_centering(coords: WordCoords[][]) {
-		let previousFillStyle;
-		if (typeof this.ctx.fillStyle === "string") {
-			previousFillStyle = this.ctx.fillStyle.toString();
-		} else {
-			previousFillStyle = Object.assign({}, this.ctx.fillStyle);
-		}
+	public adjust_text_for_centering() {
 		// performance degrader, since this parses the same loop twice.
 		// i'll fix it later.
 		const y_center_relative = Math.floor(this.height / 2);
@@ -165,33 +127,35 @@ export class TextArea {
 		const relative_x_text_center = this.relative_text_width / 2;
 		const relative_y_text_center = this.relative_text_height / 2;
 
-		const relative_text_x = this.will_center_x 
+		const relative_text_x = this.will_center_x
 			? x_center_relative - relative_x_text_center
 			: 0;
-		const relative_text_y = this.will_center_y 
+		const relative_text_y = this.will_center_y
 			? y_center_relative - relative_y_text_center
 			: 0;
 
 		this.absolute_text_x = this.x + relative_text_x;
 		this.absolute_text_y = this.y + relative_text_y;
 
-		this.ctx.fillStyle = previousFillStyle;
-
-		for (const y_coord of coords) {
+		for (const y_coord of this.coords) {
 			for (const x_coord of y_coord) {
 				x_coord.y += relative_text_y;
 				x_coord.x += relative_text_x; // relative_text_x | for real centering by x
 			}
 		}
-	}
-	public draw_center() {
-		let previousFillStyle;
-		if (typeof this.ctx.fillStyle === "string") {
-			previousFillStyle = this.ctx.fillStyle.toString();
-		} else {
-			previousFillStyle = Object.assign({}, this.ctx.fillStyle);
-		}
 
+		return this;
+	}
+	public render() {
+		for (const y_coord of this.coords) {
+			for (const x_coord of y_coord) {
+				this.ctx.fillText(x_coord.text, x_coord.x, x_coord.y);
+			}
+		}
+		return this;
+	}
+
+	public draw_center() {
 		const y_center_relative = Math.floor(this.height / 2);
 		const y_center_absolute = this.y + y_center_relative;
 		const x_center_relative = Math.floor(this.width / 2);
@@ -203,117 +167,100 @@ export class TextArea {
 		this.ctx.fillStyle = "purple";
 		this.ctx.fillRect(this.absolute_text_x, this.absolute_text_y, 5, 5);
 
-		this.ctx.fillStyle = previousFillStyle;
+		return this;
 	}
 	public draw_bounding_box() {
-		let previousStrokeStyle;
-		if (typeof this.ctx.fillStyle === "string") {
-			previousStrokeStyle = this.ctx.fillStyle.toString();
-		} else {
-			previousStrokeStyle = Object.assign({}, this.ctx.fillStyle);
-		}
-
 		this.ctx.lineWidth = 3;
 		this.ctx.beginPath();
 		this.ctx.strokeStyle = "red";
 		this.ctx.rect(this.x, this.y, this.width, this.height);
 		this.ctx.stroke();
-		this.ctx.lineWidth = 1;
 
-		this.ctx.strokeStyle = previousStrokeStyle;
+		return this;
 	}
 	public draw_true_bounding_box() {
-		let previousStrokeStyle;
-		if (typeof this.ctx.fillStyle === "string") {
-			previousStrokeStyle = this.ctx.fillStyle.toString();
-		} else {
-			previousStrokeStyle = Object.assign({}, this.ctx.fillStyle);
-		}
-
 		this.ctx.lineWidth = 3;
 		this.ctx.beginPath();
 		this.ctx.strokeStyle = "purple";
 		this.ctx.rect(this.absolute_text_x, this.absolute_text_y, this.relative_text_width, this.relative_text_height);
 		this.ctx.stroke();
-		this.ctx.lineWidth = 1;
 
-		this.ctx.strokeStyle = previousStrokeStyle;
+		return this;
 	}
 }
 
+function adjust_text(ctx: canvas.CanvasRenderingContext2D, text: Text) {
+	ctx.font = text.style ?? "";
+	ctx.fillStyle = text.color ?? "#d9d9d9";
+	const text_area = new TextArea(ctx, text.text, text.x, text.y, text.w, text.h)
+		.store_styles()
+		.wrap_words()
+		.adjust_text_for_centering()
+		.render();
+	
+	const previous_style = text_area.get_previous_style();
+	ctx.fillStyle = previous_style.fillStyle ?? ctx.fillStyle;
+	ctx.lineWidth = previous_style.lineWidth ?? ctx.lineWidth;
+	ctx.strokeStyle = previous_style.strokeStyle ?? ctx.strokeStyle;
+	ctx.font = previous_style.font ?? ctx.font;
+
+	return text_area;
+}
+
 export function quote(
-    ctx: canvas.CanvasRenderingContext2D,
-    text: Text,
-    author: Text,
-    pfp: canvas.Image,
-    overlay: canvas.Image,
+	ctx: canvas.CanvasRenderingContext2D,
+	text: Text,
+	author: Text,
+	pfp: canvas.Image,
+	overlay: canvas.Image,
 ) {
-    ctx.drawImage(pfp, 0, 0, 720, 718);
-    ctx.drawImage(overlay, 0, 0);
+	ctx.drawImage(pfp, 0, 0, 720, 718);
+	ctx.drawImage(overlay, 0, 0);
 
-    ctx.fillStyle = "#d9d9d9";
-		
-    ctx.font = `${text.extras ?? ""} ${text.size}px Times New Roman`;
-    const main_text = new TextArea(ctx, text.text, text.x, text.y, text.w, text.h);
-		const coords = main_text.wrap_words();
-		main_text.adjust_text_for_centering(coords);
-	  const main_text_coords = main_text.auto_font_resize(text.iterations, text.max_font_size);
-		for (const y_coord of main_text_coords.wrapped) {
-			for (const x_coord of y_coord) {
-				ctx.fillText(x_coord.text, x_coord.x, x_coord.y);
-			}
-		}
+	const main_text = adjust_text(ctx, text);
+	const author_text = adjust_text(ctx, author);
 
-    ctx.font = `${author.extras ?? ""} ${author.size}px Times New Roman`;
-    const author_text = new TextArea(ctx, author.text, author.x, author.y, author.w, author.h);
-		author_text.wrap_words();
-	  const author_text_coords = author_text.auto_font_resize(author.iterations, author.max_font_size);
-		author_text.adjust_text_for_centering(author_text_coords.wrapped);
-		for (const y_coord of author_text_coords.wrapped) {
-			for (const x_coord of y_coord) {
-				ctx.fillText(x_coord.text, x_coord.x, x_coord.y);
-			}
-		}
-
-		return {main_text, author_text};
+	return {main_text, author_text};
 }
 
 export function quoteAttachment(
-    c: canvas.Canvas,
-    ctx: canvas.CanvasRenderingContext2D,
-    text: Text,
-    author: Text,
-    pfp: canvas.Image,
-    overlay: canvas.Image,
-    attachment: canvas.Image | canvas.Canvas,
+	c: canvas.Canvas,
+	ctx: canvas.CanvasRenderingContext2D,
+	text: Text,
+	author: Text,
+	pfp: canvas.Image,
+	overlay: canvas.Image,
+	attachment: canvas.Image | canvas.Canvas,
 ) {
-    ctx.drawImage(pfp, 0, 0, 720, 718);
-    ctx.drawImage(overlay, 0, 0);
-    
-		const areas = quote(ctx, text, author, pfp, overlay)
+	ctx.drawImage(pfp, 0, 0, 720, 718);
+	ctx.drawImage(overlay, 0, 0);
 
-		const x = 1000;
-		const y = (c.height / 2) - 100;
-		const ratio = attachment.width / attachment.height;
-    const width = 400;
-    const height = 400;
-		let adjusted_width = width.valueOf();
-		let adjusted_height = height.valueOf();
+	const main_text = adjust_text(ctx, text);
+	const author_text = adjust_text(ctx, author);
 
-		if (width / ratio > height) {
-			adjusted_width = height * ratio;
-		} else {
-			adjusted_height = width / ratio;
-		}
+	const x = 1000;
+	const y = (c.height / 2) - 100;
+	const ratio = attachment.width / attachment.height;
+	const width = 400;
+	const height = 400;
+	let adjusted_width = width.valueOf();
+	let adjusted_height = height.valueOf();
 
-    ctx.globalAlpha = 0.9;
-    ctx.drawImage(
-        attachment, 
-        x - (adjusted_width / 2), 
-        y - (adjusted_height / 2), 
-        adjusted_width, 
-        adjusted_height);
-    ctx.globalAlpha = 1;
+	if (width / ratio > height) {
+		adjusted_width = height * ratio;
+	} else {
+		adjusted_height = width / ratio;
+	}
 
-		return areas;
+	ctx.globalAlpha = 0.9;
+	ctx.drawImage(
+		attachment,
+		x - (adjusted_width / 2),
+		y - (adjusted_height / 2),
+		adjusted_width,
+		adjusted_height
+	);
+	ctx.globalAlpha = 1;
+
+	return {main_text, author_text};
 }
